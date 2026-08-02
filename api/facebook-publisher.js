91807a9fdd05cc3624bc6c3428022738fccb0a57
Philip <contact.etbtc@gmail.com>
2026-08-02 16:55:08 +0800

    Support flexible parameter names for access token

--- api/facebook-publisher.js
+++ api/facebook-publisher.js
@@ -1,16 +1,28 @@
 // api/facebook-publisher.js
 module.exports = async function facebookPublisher(req, res) {
   try {
-    const { pageId, accessToken, cookieData, caption, cardTitle, websiteUrl } = req.body || {};
+    const body = req.body || {};
 
-    // Basic Validation
+    // Extract access token regardless of parameter casing
+    const accessToken = body.accessToken || body.access_token || body.token || body.accessTokenInput;
+    const cookieData = body.cookieData || body.cookie_data || body.cookies;
+    const pageId = body.pageId || body.page_id || body.selectedPage;
+    const caption = body.caption || body.message || '';
+    const websiteUrl = body.websiteUrl || body.website_url || body.link || '';
+
+    // Log received keys for debugging
+    console.log("Received Body Keys:", Object.keys(body));
+
     if (!accessToken) {
-      return res.status(400).json({ error: 'Missing Access Token.' });
+      return res.status(400).json({ 
+        error: 'Missing Access Token.',
+        receivedKeys: Object.keys(body)
+      });
     }
 
     const targetId = (pageId && pageId !== 'me') ? pageId : 'me';
 
-    // Post to Facebook Graph API
+    // Request to Facebook Graph API
     const fbResponse = await fetch(`https://graph.facebook.com/v18.0/${targetId}/feed`, {
       method: 'POST',
       headers: {
@@ -18,7 +30,7 @@ module.exports = async function facebookPublisher(req, res) {
         ...(cookieData ? { 'Cookie': cookieData } : {})
       },
       body: JSON.stringify({
-        message: caption || '',
+        message: caption,
         link: websiteUrl || undefined,
         access_token: accessToken
       })
